aa3763ab38c29dca922ee646c4fa8e9483f2d314
Alpha-V <alphaverta@gmail.com>
2018-05-03 19:02:22 +0200

    Refactor Command Code Into State

---
+++ src/commandstate.h
@@ -0,0 +1,64 @@
+#ifndef SRC_COMMANDSTATE_H_
+#define SRC_COMMANDSTATE_H_
+
+#include "state.h"
+
+#include "ArduinoSTL.h"
+#include <vector>
+
+/*
+ * Basic implementation for a command recieved from COSMOS
+ */
+struct __attribute__((packed)) command_t
+{
+	uint8_t id;
+
+	/*
+	 * Further parameters left up to children. NOTE: Children need to have the same order/datatypes & need to be packed
+	 */
+};
+
+/*
+ * Type used for functions that are (going to be) called when a command matching the binding's ID comes in.
+ */
+typedef void (*commandBinding)(command_t *command);
+
+/*
+ * Used for an internal representation of a command bound to a given function
+ */
+struct commandBinding_t
+{
+	commandBinding_t(command_t _command, commandBinding command_binding)
+	{
+		command = _command;
+		binding = command_binding;
+	}
+
+	command_t command;
+
+	// NOTE: Should be able to safely use the provided command as there is only one thread -> can't have a new instance while this one is still processing
+	commandBinding binding;
+};
+
+/*
+ * Command state class - will "listen" to the serial port for registered commands (sent from COSMOS) and invoke the bound functions when it receives them.
+ * NOTE - When serial data comes in (eg. serial is available) it will take (at most) the serial timeout's time & block the thread. Reccomended action is to set the serial time out to a low(-ish) value.
+ * NOTE - Commands can NOT be more than 256 bytes!!!
+ */
+class CommandState : public State
+{
+public:
+	// Constructor - sets interval & calls super to begin initialization
+	CommandState();
+	
+	// Called when this state ticks
+	virtual void Call() override;
+
+	// Registers a command to "listen" for when receiving serial data (from COSMOS)
+	void RegisterCommand(command_t command, commandBinding binding);
+
+private:
+	std::vector<commandBinding_t> commandBindings;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "state.h"
 #include "telemetrystate.h"
+#include "commandstate.h"
 
 struct lightTelemetry_t : telemetryPacket_t
 {
@@ -22,32 +23,6 @@ struct lightTelemetry_t : telemetryPacket_t
 // Create an instance of the lgiht telemetry struct so we can log LDR data to COSMOS
 lightTelemetry_t lightTelemetry;
 
-struct __attribute__((packed)) command_t
-{
-	uint8_t id;
-
-	/*
-	 * Further parameters left up to children. NOTE: Children need to have the same order/datatypes & need to be packed
-	 */
-};
-
-// Typedef the "standard" binding function so it can be typecasted/used in variables more easily + create a more uniform experience within the library
-typedef void (*commandBinding)(command_t *command);
-
-struct commandBinding_t
-{
-	commandBinding_t(command_t *_command, void (*command_binding)(command_t*))
-	{
-		command = _command;
-		binding = command_binding;
-	}
-
-	command_t *command;
-
-	// NOTE: Should be able to safely use the provided command as there is only one thread -> can't have a new instance while this one is still processing
-	commandBinding binding;
-};
-
 // Child struct of command_t
 struct __attribute__((packed)) LEDCommand_t : command_t
 {
@@ -58,10 +33,6 @@ struct __attribute__((packed)) LEDCommand_t : command_t
 
 	uint16_t value;
 };
-
-std::vector<commandBinding_t> commands;
-
-bool registered = false;
 
 void LEDBinding(LEDCommand_t *command)
 {
@@ -87,10 +58,12 @@ void setup()
 	// Register the light telemetry we defined earlier
 	telemetryState->RegisterTelemetry(&lightTelemetry, 100);
 
-	// Create a new instance of a command, and register it with the commands system
-	LEDCommand_t *LEDCommand = new LEDCommand_t;
-	commandBinding_t binding((command_t*)LEDCommand, (commandBinding)LEDBinding);
-	commands.push_back(binding);
+	// Register the command module/state
+	CommandState *commandState = new CommandState();
+	States::RegisterState(commandState);
+	
+	command_t command = LEDCommand_t();
+	commandState->RegisterCommand(command, (commandBinding)LEDBinding);
 
 	// Set a low timeout so the receiving of commands can be fast (readBytes works, while adding a delay/etc. causes issues)
 	Serial.setTimeout(10);
@@ -98,79 +71,10 @@ void setup()
 
 void loop()
 {
-	if (!registered)
-	{
-		//Serial.println(sizeof(LEDCommand_t));
-		
-
-		registered = true;
-	}
-
 	int readValue = analogRead(0);
 	
 	// Update the telemetry state/packet's light value
 	lightTelemetry.lightValue = readValue;
-	
-	// Is the serial pending data? (AKA: is there a command pending?)
-	if (Serial.available())
-	{
-		// Command pending! TODO: Should we also be sending a length parameter so if there are 2 commands "queued" they aren't interpreted as one command?
-
-		// Attempt to read the command into a buffer (NOTE: Current command length limit is 256 bytes; Other solutions resulted in there being issues receiving the buffer ;( )
-		uint8_t *buffer = (uint8_t*)malloc(256);
-		int commandLength = Serial.readBytes(buffer, 256);
-		uint8_t *commandBuffer = (uint8_t*)realloc(buffer, commandLength);
-		
-		// If we got here that means that all of the command's bytes should have come in!
-		
-		// Is the first byte (ie the ID) set?
-		if (commandLength >= 1)
-		{
-			// The command's ID should be the first byte, so extract it to a regular integer for easier usage
-			uint8_t commandID = commandBuffer[0];
-
-			for (std::vector<commandBinding_t>::iterator it = commands.begin(); it != commands.end(); it++)
-			{
-				// Is the command/binding valid?
-				if (!it->command || !it->binding)
-				{
-					// Something is wrong - flash the built in LED (13 on the Uno, my (personal) target) so we can see it even if the serial doesn't come through
-					for (int i = 0; i < 10; i++)
-					{
-						digitalWrite(13, HIGH);
-						delay(200);
-						digitalWrite(13, LOW);
-					}
-
-					// Now log the error message (TODO: Should there be some meta setup with COSMOS for these kidns of logs?)
-					Serial.print("Error with comands! Invalid command/binding! Target ID: ");
-					Serial.println(commandID);
-					
-					// Something went wrong - an invalid command or binding was set up, but this doesn't mean that there are other valid commands, so continue with the next one
-					continue;
-				}
-				
-				// Is this the command COSMOS is trying to execute?
-				if (it->command->id == commandID)
-				{					// Found the command! Execute the function, passing in the commandBuffer casted to the command_t. Assume user has set up their struct they're typecasting to correctly so it won't cause any size mismatches -> issues
-					it->binding((command_t *)commandBuffer);
-////					LEDBinding((command_t *)commandBuffer);	
-				}
-			}
-
-			// TODO Ensure the freeing of commandBuffer in all cases! We don't want any memory leaks!!!!!
-		}
-		else
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				digitalWrite(13, HIGH);
-				delay(500);
-				digitalWrite(13, LOW);
-			}
-			Serial.println("Error parsing command - length is less than 1!!!! Command appears to have been invalid, or your serial port may be occupied");
-		}
-	}
 
 	// Tick the states!
 	States::Tick();
