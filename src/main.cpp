0dcf66615f9343658525b329eddbdf7115c9e280
Alpha-V <alphaverta@gmail.com>
2018-05-03 17:39:46 +0200

    Remove most unused code

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,52 @@ struct lightTelemetry_t : telemetryPacket_t
 // Create an instance of the lgiht telemetry struct so we can log LDR data to COSMOS
 lightTelemetry_t lightTelemetry;
 
+struct __attribute__((packed)) command_t
+{
+	uint8_t id;
+
+	/*
+	 * Further parameters left up to children. NOTE: Children need to have the same order/datatypes & need to be packed
+	 */
+};
+
+// Typedef the "standard" binding function so it can be typecasted/used in variables more easily + create a more uniform experience within the library
+typedef void (*commandBinding)(command_t *command);
+
+struct commandBinding_t
+{
+	commandBinding_t(command_t *_command, void (*command_binding)(command_t*))
+	{
+		command = _command;
+		binding = command_binding;
+	}
+
+	command_t *command;
+
+	// NOTE: Should be able to safely use the provided command as there is only one thread -> can't have a new instance while this one is still processing
+	commandBinding binding;
+};
+
+// Child struct of command_t
+struct __attribute__((packed)) LEDCommand_t : command_t
+{
+	LEDCommand_t()
+	{
+		command_t::id = 1;
+	}
+
+	uint16_t value;
+};
+
+std::vector<commandBinding_t> commands;
+
+bool registered = false;
+
+void LEDBinding(LEDCommand_t *command)
+{
+	analogWrite(9, map(command->value, 0, 1023, 0, 255));
+}
+
 void setup()
 {
 
@@ -41,87 +87,25 @@ void setup()
 	// Register the light telemetry we defined earlier
 	telemetryState->RegisterTelemetry(&lightTelemetry, 100);
 
+	// Create a new instance of a command, and register it with the commands system
+	LEDCommand_t *LEDCommand = new LEDCommand_t;
+	commandBinding_t binding((command_t*)LEDCommand, (commandBinding)LEDBinding);
+	commands.push_back(binding);
+
 	// Set a low timeout so the receiving of commands can be fast (readBytes works, while adding a delay/etc. causes issues)
 	Serial.setTimeout(10);
 }
 
-struct __attribute__((packed)) command_t
-{
-	uint8_t id;
-
-	/*
-	 * Further parameters left up to children. NOTE: Children need to have the same order/datatypes & need to be packed
-	 */
-};
-
-struct commandBinding_t
-{
-	commandBinding_t(command_t *_command, void (*command_binding)(command_t*))
-	{
-		command = _command;
-		binding = command_binding;
-	}
-
-	command_t *command;
-
-	// NOTE: Should be able to safely use the provided command as there is only one thread -> can't have a new instance while this one is still processing
-	void (*binding)(command_t *command); 
-};
-
-// Child struct of command_t
-struct __attribute__((packed)) LEDCommand_t : command_t
-{
-	LEDCommand_t()
-	{
-		command_t::id = 1;
-	}
-
-	uint16_t value;
-};
-
-std::vector<commandBinding_t> commands;
-
-bool registered = false;
-
-bool lightStatus = false;
-
-void LEDBinding(LEDCommand_t *command)
-{
-	analogWrite(9, map(command->value, 0, 1023, 0, 255));
-}
-
 void loop()
 {
-/*
-	size_t size = sizeof(LEDCommand_t);
-	
-//	Serial.println(size);
-
-	LEDCommand_t command;
-	command.id = 1;
-	command.value = 543;
-	
-	const char *commandPtr = (const char*)&command;
-
-	for (int i = 0; i < size; size++)
-	{
-		Serial.write(commandPtr[i]);
-	}
-
-	delay(1000);
-}
-*/
 	if (!registered)
 	{
 		//Serial.println(sizeof(LEDCommand_t));
 		
-		LEDCommand_t *LEDCommand = new LEDCommand_t;
 
-		commandBinding_t binding((command_t*)LEDCommand, LEDBinding);
-
-		commands.push_back(binding);
 		registered = true;
 	}
+
 	int readValue = analogRead(0);
 	
 	// Update the telemetry state/packet's light value
@@ -131,87 +115,46 @@ void loop()
 	if (Serial.available())
 	{
 		// Command pending! TODO: Should we also be sending a length parameter so if there are 2 commands "queued" they aren't interpreted as one command?
-/*
-		int commandLength = 0;
 
-		size_t bufferSize = 1;
-		uint8_t *commandBuffer = (uint8_t *)malloc(bufferSize);
-
-		while (Serial.available())
-		{	
-			// Is the buffer still large enough?
-			if (commandLength + 1 > bufferSize)
-			{
-				// Welp, it isn't! Reallocate it to twice the size (appears to be good memory vs performance)
-				bufferSize *= 2;
-				commandBuffer = (uint8_t*)realloc(commandBuffer, bufferSize);
-				
-				// Double-check that the command buffer is still valid (if it isn't then we may have ran out of memory, or hit another issue, thus exiting!!!!!!)
-				if (!commandBuffer)
-				{
-					// TODO Remove this - serial print just in case we ever hit this
-					Serial.println("Error reallocating command buffer!");
-					return;
-				}
-			}
-			
-			// Now that we know there is a large enough buffer allocated, we can read the next byte and increase the buffer's size.
-			commandBuffer[bufferSize] = Serial.read();
-			commandLength++;
-
-			// TODO Is there a better way to receive all of the command>? Without this it appears to be regesting every byte/keystoke as a new command, but with this it induces a delay into the equasion
-			delay(5);
-		}
-*/
+		// Attempt to read the command into a buffer (NOTE: Current command length limit is 256 bytes; Other solutions resulted in there being issues receiving the buffer ;( )
 		uint8_t *buffer = (uint8_t*)malloc(256);
 		int commandLength = Serial.readBytes(buffer, 256);
 		uint8_t *commandBuffer = (uint8_t*)realloc(buffer, commandLength);
+		
 		// If we got here that means that all of the command's bytes should have come in!
 		
 		// Is the first byte (ie the ID) set?
 		if (commandLength >= 1)
 		{
+			// The command's ID should be the first byte, so extract it to a regular integer for easier usage
 			uint8_t commandID = commandBuffer[0];
-			
-///			Serial.print("Found command with ID: ");
-///			Serial.println(commandID);
-
-////			digitalWrite(13, HIGH);
-////			delay(1000);
-////			digitalWrite(13, LOW);
-
-///			delay(1000);
 
 			for (std::vector<commandBinding_t>::iterator it = commands.begin(); it != commands.end(); it++)
 			{
+				// Is the command/binding valid?
 				if (!it->command || !it->binding)
 				{
+					// Something is wrong - flash the built in LED (13 on the Uno, my (personal) target) so we can see it even if the serial doesn't come through
 					for (int i = 0; i < 10; i++)
 					{
 						digitalWrite(13, HIGH);
 						delay(200);
 						digitalWrite(13, LOW);
 					}
+
+					// Now log the error message (TODO: Should there be some meta setup with COSMOS for these kidns of logs?)
 					Serial.print("Error with comands! Invalid command/binding! Target ID: ");
-					Serial.print(commandID);
-					Serial.println("");
-					// Something went wrong - an invalid command or binding was set up!
-					return;
+					Serial.println(commandID);
+					
+					// Something went wrong - an invalid command or binding was set up, but this doesn't mean that there are other valid commands, so continue with the next one
+					continue;
 				}
 				
 				// Is this the command COSMOS is trying to execute?
 				if (it->command->id == commandID)
-				{
-///					Serial.println("Found command!");
-//					digitalWrite(13, HIGH);
-//					delay(1000);
-//					digitalWrite(13, LOW);
-					// Found the command! Execute the function, passing in the commandBuffer casted to the command_t. Assume user has set up their struct they're typecasting to correctly so it won't cause any size mismatches -> issues
-					////it->binding((command_t *)commandBuffer);
-					LEDBinding((command_t *)commandBuffer);
-//					LEDBinding(nullptr);
-
-					//digitalWrite(13, HIGH);
+				{					// Found the command! Execute the function, passing in the commandBuffer casted to the command_t. Assume user has set up their struct they're typecasting to correctly so it won't cause any size mismatches -> issues
+					it->binding((command_t *)commandBuffer);
+////					LEDBinding((command_t *)commandBuffer);	
 				}
 			}
 
@@ -220,18 +163,13 @@ void loop()
 		else
 		{
 			for (int i = 0; i < 10; i++)
-					{
-						digitalWrite(13, HIGH);
-						delay(500);
-						digitalWrite(13, LOW);
-					}
+			{
+				digitalWrite(13, HIGH);
+				delay(500);
+				digitalWrite(13, LOW);
+			}
+			Serial.println("Error parsing command - length is less than 1!!!! Command appears to have been invalid, or your serial port may be occupied");
 		}
-
-///	
-
-///		Serial.println("Received Command!");
-///		Serial.println(commandLength);
-
 	}
 
 	// Tick the states!
